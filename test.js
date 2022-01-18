6b070ca88083c802dc721c9de53a32d657930c48
rcoelho-aka <ricardo.coelho@akapeople.pt>
2022-01-18 01:42:47 +0000

    Initial commit

---
+++ test.js
@@ -0,0 +1,16 @@
+var assert = require('assert');
+
+describe('Dummy test cases', function() {
+  describe('Test Case A', function() {
+    it('Should always pass', function() {
+      return true
+    });
+  });
+
+  describe('Test Case B', function() {
+    it('Should also always pass', function() {
+      return true
+    });
+  });
+
+});
