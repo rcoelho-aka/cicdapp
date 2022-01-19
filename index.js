665ac69f2d4fc97ae2acec101a2acfe05ae92ffe
braganm2 <75120705+braganm2@users.noreply.github.com>
2022-01-19 19:38:27 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ app.listen(port, () => {
 })
 
 
-// const add = (a = 0, b = 0) => {
-//   if (typeof a !== 'number' || typeof b !== 'number') {
-//     return 0;
-//   }
-//   return a + b;
-// };
+const add = (a = 0, b = 0) => {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    return 0;
+  }
+  return a + b;
+};
 
