3693c05242ca6e3c38bb937de7c067bb74233d1c
cruzluis69 <92943742+cruzluis69@users.noreply.github.com>
2022-01-19 19:23:19 +0000

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,9 +1,63 @@
 pipeline {
     agent any
-    stages ('Exemple') {
-        steps {
-            echo 'Hello World'
+
+    stages {
+
+        stage('Setup') {
+            steps {
+                script {
+                    env.TAG = "registry.heroku.com/${env.JOB_NAME}/web"
+                }
+            }
         }
+
+        stage('Checkout') {
+            steps {
+                checkout scm
+            }
+        }
+
+        stage('Audit') {
+            steps {
+                sh 'npm audit'
+            }
+        }
+
+        stage('Install') {
+            steps {
+                sh 'npm install'
+            }
+        }
+
+        stage('Unit tests') {
+            steps {
+                sh 'npm test'
+            }
+        }
+
+        stage('Build docker image') {
+            steps {
+                sh "docker build -t ${TAG} ."
+            }
+        }
+
+        stage('Push to repository') {
+            steps {
+                withCredentials([string(credentialsId: 'heroku-key', variable: 'HEROKU_API_KEY')]) {
+                    sh 'heroku container:login'
+                    sh "docker push ${TAG}"
+                }
+            }
+        }
+
+        stage('Deploy') {
+            steps {
+                withCredentials([string(credentialsId: 'heroku-key', variable: 'HEROKU_API_KEY')]) {
+                    sh "heroku container:release web -a ${env.JOB_NAME}"
+                    sh "heroku config:set VERSION=${env.BUILD_NUMBER} -a ${env.JOB_NAME}"
+                }
+            }
+        }
+
     }
-}
 }
