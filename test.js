621234d1eb4d2ec656f9138028de68b3f7654a5c
braganm2 <75120705+braganm2@users.noreply.github.com>
2022-01-19 19:48:32 +0000

    error commented final

--- test.js
+++ test.js
@@ -11,20 +11,10 @@ describe('Dummy test cases', function () {
     it('Should also always pass', function () {
       assert(true)
     });
-    it('Should also not pass', function () {
-      assert(false)
-    });
+    // //to cause error
+    // it('Should also not pass', function () {
+    //   assert(false)
+    // });
   });
 
 });
-
-// //To test the error in UnitTest Stage
-// const { add } = require('index');
-
-// describe("Add function", () => {
-//   it("should return 2 added numbers", () => {
-//     const result = add(2, 2);
-//     //expect(result).to.be.eq(5);//to cause error
-//     expect(result).to.be.eq(4);
-//   });
-// });
