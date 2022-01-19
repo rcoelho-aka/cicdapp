9669e28c75d5f1d66e80e33c0d60cb2877b40a14
pereirafm <86659573+pereirafm@users.noreply.github.com>
2022-01-19 21:01:20 +0000

    Add files via upload

--- test.js
+++ test.js
@@ -1,16 +1,11 @@
 var assert = require('assert');
 
-describe('Dummy test cases', function() {
-  describe('Test Case A', function() {
-    it('Should always pass', function() {
-      assert(true)
+describe('Test Case B', function () {
+    it('Should also always pass', function () {
+    assert(true)
     });
-  });
-
-  describe('Test Case B', function() {
-    it('Should also always pass', function() {
-      assert(true)
-    });
-  });
-
+    // //to cause error
+    // it('Should also not pass', function () {
+    //   assert(false)
+    // });
 });
