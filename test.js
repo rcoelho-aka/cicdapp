af32d217e9a5a52dd3251b185713947ca06f095c
braganm2 <75120705+braganm2@users.noreply.github.com>
2022-01-19 19:30:18 +0000

    error Added

--- test.js
+++ test.js
@@ -1,16 +1,23 @@
 var assert = require('assert');
 
-describe('Dummy test cases', function() {
-  describe('Test Case A', function() {
-    it('Should always pass', function() {
+describe('Dummy test cases', function () {
+  describe('Test Case A', function () {
+    it('Should always pass', function () {
       assert(true)
     });
   });
 
-  describe('Test Case B', function() {
-    it('Should also always pass', function() {
+  describe('Test Case B', function () {
+    it('Should also always pass', function () {
       assert(true)
     });
   });
 
+});
+
+describe("Add function", () => {
+  it("should return 2 added numbers", () => {
+    const result = add(2, 2);
+    expect(result).to.be.eq(5);
+  });
 });
