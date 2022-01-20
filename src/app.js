abdc511659b69da82ee4c21d0a68ad1eacb7327b
moraisp2 <pedro.morais2@vodafone.com>
2022-01-20 19:01:00 +0000

    moraisp2 code review

--- src/app.js
+++ src/app.js
@@ -1,21 +1,19 @@
-const express = require('express')
-const app = express()
-const version = process.env.VERSION || 'local'
+const express = require('express');
+const app = express();
+const version = process.env.VERSION || 'local';
 
 
 const getMessage = () => {
-  const message = `Hello World! \n Version: ${version}`
-  const goodMessage = message.startsWith('Hello')
+  const message = `Hello World! \n Version: ${version}`;
 
-  if (goodMessage == true)
-    return message
+  if (message.startsWith('Hello'))
+    return message;
 
-  console.log(message)
-}
+  console.log(message);
+};
 
 app.get('/', (req, res) => {
-  const message = getMessage()
-  res.send(getMessage())
-})
+  res.send(getMessage());
+});
 
 module.exports = app;
