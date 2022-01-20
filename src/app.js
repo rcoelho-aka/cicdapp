0261683a7ff444f2e03c875d84b378eb8c9d286f
cardojp5 <joao.cardoso01@vodafone.com>
2022-01-20 18:38:32 +0000

    correctSonar

--- src/app.js
+++ src/app.js
@@ -6,15 +6,11 @@ const version = process.env.VERSION || 'local'
 const getMessage = () => {
   const message = `Hello World! \n Version: ${version}`
   const goodMessage = message.startsWith('Hello')
-
-  if (goodMessage == true)
-    return message
-
   console.log(message)
+  return message
 }
 
 app.get('/', (req, res) => {
-  const message = getMessage()
   res.send(getMessage())
 })
 
