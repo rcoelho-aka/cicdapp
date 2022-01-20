2583b4eedcad25838dcbc05fecb3c7abdb6f90ba
miguel.goncalves3 <miguel.goncalves3@vodafone.com>
2022-01-20 20:14:56 +0000

    app

--- src/app.js
+++ src/app.js
@@ -4,13 +4,14 @@ const version = process.env.VERSION || 'local'
 
 
 const getMessage = () => {
-  const message = `Hello World! \n Version: ${version}`
-  const goodMessage = message.startsWith('Hello')
+  console.log("Hello World!")
+  //const message = `Hello World! \n Version: ${version}`
+  //const goodMessage = message.startsWith('Hello')
 
   //if (goodMessage == true)
-    return message
+    //return message
 
-  console.log(message)
+ // console.log(message)
 }
 
 app.get('/', (req, res) => {
