da3693ff18e75842b3109081dd213d49c9d8978d
nevesj1 <joao.neves1@vodafone.com>
2022-01-21 14:09:44 +0000

    vdv-aka-04-ex2

--- src/app.js
+++ src/app.js
@@ -4,18 +4,11 @@ const version = process.env.VERSION || 'local'
 
 
 const getMessage = () => {
-  const message = `Hello World! \n Version: ${version}`
-  const goodMessage = message.startsWith('Hello')
 
-  if (goodMessage == true)
-    return message
-
-  console.log(message)
 }
 
 app.get('/', (req, res) => {
-  const message = getMessage()
-  res.send(getMessage())
+
 })
 
 module.exports = app;
